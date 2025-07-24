b7245b13c27f1eaeb59d023323f4756fc4f70977
Meir Weissman <shira05041@gmail.com>
2025-07-24 12:13:11 +0300

    update main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.routers import auth
+from app.schemas import project, team, user, task, comment
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    debug=settings.DEBUG
+)
 
-app.include_router(auth.router)
+app.include_router(auth.router, prefix=settings.API_V1_STR, tags=["auth"])
